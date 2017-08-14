ed6f52d5c2cb199977a2b4042c0c849dce1cbfaf
Wasif Zaman <teatocode@gmail.com>
2017-08-14 12:05:56 -0400

    set up project

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const nunjucks = require('nunjucks');
+
+const app = express();
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render);
+nunjucks.configure('views', { noCache: true });
+
+app.use(express.static('css'));
+app.get('/', (req, res)=> {
+	res.render('index');
+})
+
+const port = process.env.PORT || 3000;
+app.listen(port, ()=> {
+	console.log(`listening n port ${port}`);
+})
